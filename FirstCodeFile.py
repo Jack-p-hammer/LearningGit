8716f3ff2a7d7282387d43484aa5b956b2f96a3e
Jack-p-hammer <jonathan.hammer3@gmail.com>
2025-04-19 13:05:51 -0400

    Included controls parameters

--- FirstCodeFile.py
+++ FirstCodeFile.py
@@ -2,8 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x = np.linspace(0, 10, 100)
-y = 1 + (0-1)*np.cos(x)*np.exp(-x/3)
+x = np.linspace(0, 15, 100)
+# Generate a sine wave with an exponential decay
+v0 = 0
+v1 = 1
+wn = .5
+zeta = 0.25
+y = v1 + (v0-v1)*np.cos(wn*x)*np.exp(-zeta*x)
 
 plt.plot(x, y)
 plt.title('Sine Wave')
