460b9ed923f8f8bfda2b95a47e92203ad1810569
Jack-p-hammer <jonathan.hammer3@gmail.com>
2025-04-19 14:42:35 -0400

    Toooo repo

--- FirstCodeFile.py
+++ FirstCodeFile.py
@@ -6,8 +6,8 @@ x = np.linspace(0, 15, 100)
 # Generate a sine wave with an exponential decay
 v0 = 0
 v1 = 1
-wn = .5
-zeta = 0.25
+wn = .2
+zeta = 0.05
 y = v1 + (v0-v1)*np.cos(wn*x)*np.exp(-zeta*x)
 
 plt.plot(x, y)
